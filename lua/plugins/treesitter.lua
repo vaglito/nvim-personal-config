0869f505a73322a09ed71f80ee06c5f59387f854
Cesar Gonzalez <cesargonzalez390@gmail.com>
2025-10-09 10:22:11 -0500

    add markdown and markdown_inlines

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
     config = function()
         local configs = require("nvim-treesitter.configs")
         configs.setup({
-            ensure_installed = {"lua", "c", "javascript", "python", "typescript", "css", "html"},
+            ensure_installed = {"lua", "c", "javascript", "python", "typescript", "css", "html", "markdown", "markdown_inline"},
             highlight = { enable = true },
             indent = { enable = true }
         })
