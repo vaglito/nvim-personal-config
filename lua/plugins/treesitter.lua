5094e6bf069e463870a0dbe70293df0ee5624e3c
Cesar Gonzalez <cesargonzalez390@gmail.com>
2025-10-09 10:31:16 -0500

    fix: bug mirror

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,7 +5,7 @@ return {
         local configs = require("nvim-treesitter.configs")
         configs.setup({
             ensure_installed = {"lua", "c", "javascript", "python", "typescript", "css", "html", "markdown", "markdown_inline"},
-            highlight = { enable = true },
+            highlight = { enable = true, additional_vim_regex_highlighting = false },
             indent = { enable = true }
         })
     end
