bbec81ce3e8579d4ba81c5bcc7d520b172b5b0f1
Cesar Enrique Gonzalez Carvajal <vaglito.dev@gmail.com>
2026-07-27 13:56:33 -0500

    feat: implement v3

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,7 @@
 vim.keymap.set("n", "gb", ":bn<CR>", {noremap = true, silent = true})
 vim.keymap.set("n", "gB", ":bp<CR>", {noremap = true, silent = true})
 vim.keymap.set("n", "gd", ":bd<CR>", {noremap = true, silent = true})
+
+vim.keymap.set({"n", "v"}, "<leader>f", function()
+	require("conform").format({ async = true, lsp_fallback = true })
+end, { desc = "Formatear archivo o seleccion"})
