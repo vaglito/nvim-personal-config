5215be405947c335bb5cbf932cd80097734a7554
Cesar Gonzalez <vaglito.dev@gmail.com>
2026-09-25 12:26:54 -0500

    feat: configure snacks statuscolumn, gitsigns and lualine

---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,22 @@
+return {
+	"nvim-lualine/lualine.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	event = "VeryLazy",
+	opts = {
+		options = {
+			theme = "auto",
+			globalstatus = true,
+			disabled_filetypes = {
+				statusline = { "snacks_dashboard", "dashboard" },
+			},
+		},
+		sections = {
+			lualine_a = { "mode" },
+			lualine_b = { "branch", "diff", "diagnostics" },
+			lualine_c = { { "filename", path = 1 } },
+			lualine_x = { "encoding", "fileformat", "filetype" },
+			lualine_y = { "progress" },
+			lualine_z = { "location" },
+		},
+	},
+}
